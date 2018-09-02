8f89ab812e5b7d4893cc0349043517b7c47691cb
Hai <hailh7@fsoft.com.vn>
2018-09-02 13:03:55 +0700

    Hai: this commit will add a controller's index method

--- src/main/java/com/example/Controller/EmployeeController.java
+++ src/main/java/com/example/Controller/EmployeeController.java
@@ -1,5 +1,16 @@
 package com.example.Controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class EmployeeController {
 
+	
+	@RequestMapping("/")
+	public String indexPage()
+	{
+		return "Hello World";
+	}
 }
